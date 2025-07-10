06130042ea673f4c45066fcd3a2e4daf5374d82b
Rebekah Davis <bekadavis9@gmail.com>
2025-07-10 13:00:33 -0400

    Class Windows inherits FilesystemBase.

--- tiledb/sm/filesystem/vfs.cc
+++ tiledb/sm/filesystem/vfs.cc
@@ -93,7 +93,7 @@ VFS::VFS(
 #endif
 
 #ifdef _WIN32
-  throw_if_not_ok(win_.init(config_));
+  win_ = Win(config_);
 #else
   posix_ = Posix(config_);
 #endif
@@ -148,7 +148,8 @@ Status VFS::create_dir(const URI& uri) const {
 
   if (uri.is_file()) {
 #ifdef _WIN32
-    return win_.create_dir(uri.to_path());
+    win_.create_dir(uri);
+    return Status::Ok();
 #else
     posix_.create_dir(uri);
     return Status::Ok();
@@ -218,7 +219,8 @@ Status VFS::touch(const URI& uri) const {
   auto instrument = make_log_duration_instrument(uri, "touch");
   if (uri.is_file()) {
 #ifdef _WIN32
-    return win_.touch(uri.to_path());
+    win_.touch(uri);
+    return Status::Ok();
 #else
     posix_.touch(uri);
     return Status::Ok();
@@ -375,7 +377,8 @@ Status VFS::remove_dir(const URI& uri) const {
   auto instrument = make_log_duration_instrument(uri, "remove_dir");
   if (uri.is_file()) {
 #ifdef _WIN32
-    return win_.remove_dir(uri.to_path());
+    win_.remove_dir(uri);
+    return Status::Ok();
 #else
     posix_.remove_dir(uri);
 #endif
@@ -433,7 +436,8 @@ Status VFS::remove_file(const URI& uri) const {
   auto instrument = make_log_duration_instrument(uri, "remove_file");
   if (uri.is_file()) {
 #ifdef _WIN32
-    return win_.remove_file(uri.to_path());
+    win_.remove_file(uri);
+    return Status::Ok();
 #else
     posix_.remove_file(uri);
     return Status::Ok();
@@ -506,9 +510,7 @@ uint64_t VFS::file_size(const URI& uri) const {
   stats_->add_counter("file_size_num", 1);
   if (uri.is_file()) {
 #ifdef _WIN32
-    uint64_t size;
-    throw_if_not_ok(win_.file_size(uri.to_path(), &size));
-    return size;
+    return win_.file_size(uri);
 #else
     return posix_.file_size(uri);
 #endif
@@ -544,7 +546,7 @@ Status VFS::is_dir(const URI& uri, bool* is_dir) const {
   auto instrument = make_log_duration_instrument(uri, "is_dir");
   if (uri.is_file()) {
 #ifdef _WIN32
-    *is_dir = win_.is_dir(uri.to_path());
+    *is_dir = win_.is_dir(uri);
 #else
     *is_dir = posix_.is_dir(uri);
 #endif
@@ -587,7 +589,7 @@ Status VFS::is_file(const URI& uri, bool* is_file) const {
   stats_->add_counter("is_object_num", 1);
   if (uri.is_file()) {
 #ifdef _WIN32
-    *is_file = win_.is_file(uri.to_path());
+    *is_file = win_.is_file(uri);
 #else
     *is_file = posix_.is_file(uri);
 #endif
@@ -736,7 +738,8 @@ Status VFS::move_file(const URI& old_uri, const URI& new_uri) {
   if (old_uri.is_file()) {
     if (new_uri.is_file()) {
 #ifdef _WIN32
-      return win_.move_path(old_uri.to_path(), new_uri.to_path());
+      win_.move_file(old_uri, new_uri);
+      return Status::Ok();
 #else
       posix_.move_file(old_uri, new_uri);
       return Status::Ok();
@@ -798,7 +801,8 @@ Status VFS::move_dir(const URI& old_uri, const URI& new_uri) {
   if (old_uri.is_file()) {
     if (new_uri.is_file()) {
 #ifdef _WIN32
-      return win_.move_path(old_uri.to_path(), new_uri.to_path());
+      win_.move_dir(old_uri, new_uri);
+      return Status::Ok();
 #else
       posix_.move_dir(old_uri, new_uri);
       return Status::Ok();
@@ -1052,7 +1056,8 @@ Status VFS::read_impl(
 
   if (uri.is_file()) {
 #ifdef _WIN32
-    return win_.read(uri.to_path(), offset, buffer, nbytes);
+    win_.read(uri, offset, buffer, nbytes, false);
+    return Status::Ok();
 #else
     posix_.read(uri, offset, buffer, nbytes, false);
     return Status::Ok();
@@ -1202,7 +1207,8 @@ Status VFS::sync(const URI& uri) {
   auto instrument = make_log_duration_instrument(uri, "sync");
   if (uri.is_file()) {
 #ifdef _WIN32
-    return win_.sync(uri.to_path());
+    win_.sync(uri);
+    return Status::Ok();
 #else
     posix_.sync(uri);
     return Status::Ok();
@@ -1340,7 +1346,8 @@ Status VFS::write(
 
   if (uri.is_file()) {
 #ifdef _WIN32
-    return win_.write(uri.to_path(), buffer, buffer_size);
+    win_.write(uri, buffer, buffer_size, remote_global_order_write);
+    return Status::Ok();
 #else
     posix_.write(uri, buffer, buffer_size, remote_global_order_write);
     return Status::Ok();
